2a988f38e52d63569b71821282307b7dd53b76a0
IMU <IMU@DESKTOP-FSRQE0S>
2023-03-28 12:26:30 +0630

    commitmessages

---
+++ src/test/java/helloworldpac/TestHelloWorld.java
@@ -0,0 +1,17 @@
+package helloworldpac;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+
+	/*
+	 * public static void main(String[] args) { // TODO Auto-generated method stub
+	 * 
+	 * }
+	 */
+	@Test
+public void TestHelloWorld()
+{
+	System.out.println("Hello!!!!!!!");
+}
+}
